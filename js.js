2825534160f7dd92bf24cd25dc32d2219ea8c2da
mdefatimaperez <77554434+mdefatimaperez@users.noreply.github.com>
2021-11-05 18:06:29 -0300

    Proyecto Final
    Entrega de Proyecto Final

---
+++ js.js
@@ -0,0 +1,29 @@
+/*CARTA PASEO*/
+function toggleCartaPaseo(){
+    var element = document.getElementById("cartaPaseo");
+    element.classList.toggle("cartaPaseo");
+    element.classList.toggle("lomoCarta1");
+}
+  
+/*CARTA YOUTUBE*/
+function toggleCartaYouTube(){
+    var element = document.getElementById("cartaYouTube");
+    element.classList.toggle("cartaYouTube");
+    element.classList.toggle("lomoCarta2");
+}
+
+
+/*CARTA PUNTO*/
+function toggleCartaPunto(){
+    var element = document.getElementById("cartaPunto");
+    element.classList.toggle("cartaPunto");
+    element.classList.toggle("lomoCarta3");
+}
+
+
+/*CARTA GAME*/
+function toggleCartaGame(){
+    var element = document.getElementById("cartaGame");
+    element.classList.toggle("cartaGame");
+    element.classList.toggle("lomoCarta4");
+}
